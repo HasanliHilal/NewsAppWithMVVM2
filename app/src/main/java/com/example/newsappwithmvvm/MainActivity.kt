c9740282216cc68dda54ddcd6b5167d13dc98036
HasanliHilal <hasanlihilal@gmail.com>
2022-06-21 17:44:48 +0400

    AddNewsRecylerviewWithDiffUtil

--- app/src/main/java/com/example/newsappwithmvvm/MainActivity.kt
+++ app/src/main/java/com/example/newsappwithmvvm/MainActivity.kt
@@ -3,20 +3,34 @@ package com.example.newsappwithmvvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentContainerView
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsappwithmvvm.databinding.ActivityMainBinding
+import com.example.newsappwithmvvm.presentation.adapter.NewsAdapter
+import com.example.newsappwithmvvm.presentation.viewmodel.NewsViewModel
+import com.example.newsappwithmvvm.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    @Inject
+    lateinit var newsAdapter:NewsAdapter
+    lateinit var viewModel: NewsViewModel
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         binding.bnvNews.setupWithNavController(
-           navHostFragment.navController
+            navHostFragment.navController
         )
+        viewModel = ViewModelProvider(this, factory).get(NewsViewModel::class.java)
     }
 }
